2dd677ab976760233b9762d05135aef5aba48853
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-29 04:52:51 +0000

    Add default displays

--- main.py
+++ main.py
@@ -1,54 +1,11 @@
-from logo import logo
-import random
+"""
+This is a number guessing game, a random number between 1 and 100 is selected by the computer.
+The user will have to guess the number by choosing a level that will determine how many attempts
+they have to guess it right.
+"""
 
-EASY = 10
-HARD = 5
-
-def level():
-  """
-  This function is used to set the game level.
-  """
-  choose = input('Choose a level, "easy" or "hard": ').lower()
-  if choose == "hard":
-    return HARD
-  else:
-    return EASY
-
-def check_guess(random_number, guess, attempts):
-  """
-  This function is used verify if guessed number is the same as the random number.
-  """
-  if guess > random_number:
-    print("Too high, Try Again.")
-    return attempts - 1
-  elif guess < random_number:
-    print("Too low, Try Again.")
-    return attempts - 1
-  else:
-    print("Right guess, you win.")
-
-def start_game():
-  """
-  This function is start the game functionality.
-  """
-  print(logo)
-  print("Guess a number between 1 and 100")
-  random_number = random.randint(1, 101)
-
-  attempts = level()
-  guess = 0
-
-  while guess != random_number:
-    print(f"You have {attempts} attempts to guess the number.")
-
-    guess = int(input("Your guess: "))
-  
-    attempts = check_guess(random_number, guess, attempts)
-
-    if attempts == 0:
-      print("You've run out of guesses, you lose.")
-      return
-    elif guess != random_number:
-      print("Guess again.")
-
-start_game()
+# Default displays
+print('IMPORTANT:\n A number between 1 and 100 is chosen at random. '
+      '\n Select a level (Easy or Hard).'
+      '\n Easy means 7 attempts.'
+      '\n Hard means 4 attempts.\n')
