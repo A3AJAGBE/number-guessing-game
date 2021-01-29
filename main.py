b2963b7b181c776245a86d132d400d3c4b85c754
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-29 05:20:44 +0000

    Add constants and level function

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ they have to guess it right.
 # Imports
 import random
 
+# constants
+EASY = 7
+HARD = 4
+
+
+# Determine the user attempts
+def level():
+    """
+    This function is used to prompt user for their desired game level.
+    """
+    choose = input('Choose a level, "easy" or "hard": ').lower()
+    if choose == "hard":
+        return HARD
+    elif choose == "easy":
+        return EASY
+    else:
+        print('Invalid response.')
+
+
 # Default displays
 print('IMPORTANT:\n A number between 1 and 100 is chosen at random. '
       '\n Select a level (Easy or Hard).'
@@ -19,16 +38,24 @@ print(random_number)
 # Add continuous iteration until declared otherwise
 game_over = False
 while not game_over:
-    # User guessing
-    guess = int(input('\nGuess a number: '))
+    attempts = level()
 
-    if guess < 1 or guess > 100:
-        print('Your guess must be between 1 and 100.')
+    # Prevent the game from running
+    if attempts is None:
+        game_over = True
     else:
-        if random_number == guess:
-            print('The guess is correct. Well done')
-            game_over = True
-        elif random_number < guess:
-            print('The guess is too high. Try again!!!')
+        print(f"You have {attempts} attempts to guess the number.")
+
+        # User guessing
+        guess = int(input('\nGuess a number: '))
+
+        if guess < 1 or guess > 100:
+            print('Your guess must be between 1 and 100.')
         else:
-            print('The guess is too low. Try again!!!')
+            if random_number == guess:
+                print(f'Correct guess, the random number was {random_number}. Well done')
+                game_over = True
+            elif random_number < guess:
+                print('The guess is too high. Try again!!!')
+            else:
+                print('The guess is too low. Try again!!!')
