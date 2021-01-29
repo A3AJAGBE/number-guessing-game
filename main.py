aa6c44354aafa895c6cc4ac47dc3c608225ebb78
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-29 05:51:00 +0000

    Add start game function

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def level():
     """
     This function is used to prompt user for their desired game level.
     """
-    choose = input('Choose a level, "easy" or "hard": ').lower()
+    choose = input('\nChoose a level, "easy" or "hard": ').lower()
     if choose == "hard":
         return HARD
     elif choose == "easy":
@@ -25,37 +25,63 @@ def level():
         print('Invalid response.')
 
 
-# Default displays
-print('IMPORTANT:\n A number between 1 and 100 is chosen at random. '
-      '\n Select a level (Easy or Hard).'
-      '\n Easy means 7 attempts.'
-      '\n Hard means 4 attempts.\n')
+# Validate the guess
+def check_guess(rand_num, guess_num, chances):
+    """
+    This function checks if guessed number is the same as the random number.
+    """
+    if guess_num > rand_num:
+        print('The guess is too high.')
+        return chances - 1
+    elif guess_num < rand_num:
+        print('The guess is too low.')
+        return chances - 1
+    else:
+        print(f'Correct guess, the random number was {rand_num}. Well done')
 
-# Random number chosen
-random_number = random.randint(1, 100)
-print(random_number)
 
-# Add continuous iteration until declared otherwise
-game_over = False
-while not game_over:
+# Game application
+def start_game():
+    """
+    This function is start the game functionality.
+    """
+    # Default displays
+    print('IMPORTANT:\n A number between 1 and 100 is chosen at random. '
+          '\n Select a level (Easy or Hard).'
+          '\n Easy means 7 attempts.'
+          '\n Hard means 4 attempts.\n')
+
+    # Random number chosen
+    random_number = random.randint(1, 100)
+    print(random_number)
+
     attempts = level()
 
-    # Prevent the game from running
-    if attempts is None:
-        game_over = True
-    else:
-        print(f"You have {attempts} attempts to guess the number.")
+    # Add continuous iteration until declared otherwise
+    game_over = False
+    while not game_over:
+        if attempts == 1:
+            print(f"You have {attempts} attempt to guess the number.")
+        else:
+            print(f"You have {attempts} attempts to guess the number.")
 
         # User guessing
         guess = int(input('\nGuess a number: '))
 
+        # Check that the user guess is within the constraints
         if guess < 1 or guess > 100:
             print('Your guess must be between 1 and 100.')
         else:
-            if random_number == guess:
-                print(f'Correct guess, the random number was {random_number}. Well done')
+            attempts = check_guess(random_number, guess, attempts)
+
+            # Prevent the game from running when
+            if attempts is None:
                 game_over = True
-            elif random_number < guess:
-                print('The guess is too high. Try again!!!')
+            elif attempts == 0:
+                print("You've run out of attempts, you lose.")
+                game_over = True
             else:
-                print('The guess is too low. Try again!!!')
+                print("Guess again.")
+
+
+start_game()
